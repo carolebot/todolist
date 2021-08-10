6a90ff27aa07f39fa754821b61272013dc7a5318
carolebot <carol.tang79@gmail.com>
2021-08-11 01:21:11 +0800

    add mongoose

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = express()
 // set localhost port
 const port = 3000
 
+// set db
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todolist')
+const db = mongoose.connection
+
+db.once('open', ()=> {
+  console.log('mongodb connected')
+})
+
+db.on('error', () => {
+  console.log('mongodb err')
+})
+
 // set template engine
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
