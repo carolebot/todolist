0c5a9c694e117e940685f0ecc547089b2aa47acb
carolebot <carol.tang79@gmail.com>
2021-09-08 12:07:44 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../../models/todo')
 router.get('/', (req, res) => {
-
-  // 拿到全部的todo資料
-  Todo.find()
+  const userId = req.user._id
+  console.log(userId)
+  // 拿到全部的todo資料(userId:userId)
+  Todo.find({ userId })
     .lean() //mongoose轉JS可處理格式
     .sort({ name: 'asc' }) //desc
     .then(todos => res.render('index', { todos })) //陣列傳到index
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,50 +8,56 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const name = req.body.name
+  const userId = req.user._id
   console.log(req.body)
   // const todo = new Todo({ name })
   // todo.save()
   //   .then(() => res.redirect('/'))
   //   .catch(error => console.log(error))
-  Todo.create({ name })
+  Todo.create({ name, userId })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
+  const _id = req.params.id
+  const userId = req.user._id
   // console.log(id)
-  Todo.findById(id)
+  Todo.findById({_id, userId})
     .lean()
     .then((todo) => res.render('details', { todo }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+
+  Todo.findById({ _id, userId })
     .lean()
     .then((todo) => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const _id = req.params.id
+  const userId = req.user._id
   // 解構賦值
   const { name, isDone } = req.body
-  Todo.findById(id)
+  Todo.findOne({userId, _id})
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       todo.save() //save是mongoose資料庫的方法
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch(error => console.log(error))
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id) //確保資料存在
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ userId, _id }) //確保資料存在
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
